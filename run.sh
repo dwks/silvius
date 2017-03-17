9e4182faa22021c1ae0fa26ca00a64f724464c47
dwk <dwks42@gmail.com>
2017-03-17 12:02:00 -0400

    Rewrite run.sh, update with -G option to show audio gate levels.
    Update README to mention audio gate, mailing list, etc.

--- run.sh
+++ run.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-if [[ $1 == '' || $1 == '-h' || $1 == '--help' ]]; then
-    echo "usage: $0 -h | --help | -e | --execute | -t | --test | -d | --delete"
-    echo -e "\t-h|--help: prints this help message"
-    echo -e "\t-e|--execute: executes results of speech recognition"
-    echo -e "\t-t|--test: prints results of speech recognition"
-    echo -e "\t-d|--delete: deletes saved microphone selection"
-    exit 0
-fi
+action=$1
+shift
+args=$@
 
-if [[ $1 != '-d' && $1 != '--delete' && $1 != '-t' && $1 != '--test' && $1 != '-e' && $1 != '--execute' ]]; then
-    echo "Unknown command '$1'. Run with --help to see usage."
-    exit 1
-fi
+function check_microphone {
+    if [[ -f .selected-mic ]]; then
+        which=$(cat .selected-mic)
+    else
+        echo "No microphone selected..."
+        echo "Finding microphones . . ."
+        python stream/list-mics.py 2>/dev/null
 
-if [[ $1 == '-d' || $1 == '--delete' ]]; then
-    rm -f .selected-mic
-    exit 0
-fi
+        echo "Use which device?"
+        read which
+        echo $which >> .selected-mic
+    fi
+    echo "Selected microphone $which."
+}
 
-if [[ -f .selected-mic ]]; then
-    which=$(cat .selected-mic)
-else
-    echo "No microphone selected..."
-    echo "Finding microphones . . ."
-    python stream/list-mics.py 2>/dev/null
+function run_recognition {
+    if [[ $1 == 1 ]]; then
+        python stream/mic.py -s silvius-server.voxhub.io -d $which $args | python grammar/main.py
+    else
+        python stream/mic.py -s silvius-server.voxhub.io -d $which $args
+    fi
+}
 
-    echo "Use which device?"
-    read which
-    echo $which >> .selected-mic
-fi
-
-echo "Selected microphone $which."
-
-if [[ $1 == '-e' || $1 == '--execute' ]]; then
-    python stream/mic.py -s silvius-server.voxhub.io -d $which | python grammar/main.py
-elif [[ $1 == '-t' || $1 == '--test' ]]; then
-    python stream/mic.py -s silvius-server.voxhub.io -d $which
-fi
+case "$action" in
+    ''|-h|--help)
+        echo "usage: $0 (-h|--help | -e|--execute | -t|--test | -d|--delete) [extra-args]"
+        echo -e "\t-h|--help: prints this help message"
+        echo -e "\t-e|--execute: executes results of speech recognition"
+        echo -e "\t-t|--test: prints results of speech recognition"
+        echo -e "\t-d|--delete: deletes saved microphone selection"
+        echo -e "\t-G|--show-gate: displays audio level of background noise"
+        ;;
+    -t|--test)
+        check_microphone
+        run_recognition 0
+        ;;
+    -e|--execute)
+        check_microphone
+        run_recognition 1
+        ;;
+    -d|--delete)
+        rm -f .selected-mic
+        ;;
+    -G|--show-gate)
+        python stream/audio-gate-level.py
+        ;;
+    *)
+        echo "Unknown command '$action'. Run with --help to see usage."
+        exit 1
+        ;;
+esac
