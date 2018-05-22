4d57276833bda129f4c8bd66eb51be17ebd187d3
dwk <dwks42@gmail.com>
2018-05-21 20:36:29 -0400

    Allow numbers to be included in phrase/sentence (since they aren't commands).
    For example, "sentence five".

--- grammar/parse.py
+++ grammar/parse.py
@@ -264,15 +264,33 @@ class CoreParser(GenericParser):
 
     def p_word_repeat(self, args):
         '''
-            word_repeat ::= ANY
-            word_repeat ::= ANY word_repeat
+            word_repeat ::= raw_word
+            word_repeat ::= raw_word word_repeat
         '''
         if(len(args) == 1):
             return AST('word_sequence', None,
-                [ AST('null', args[0].extra) ])
+                [ AST('null', args[0]) ])
         else:
-            args[1].children.insert(0, AST('null', args[0].extra))
+            args[1].children.insert(0, AST('null', args[0]))
             return args[1]
+
+    def p_raw_word(self, args):
+        '''
+            raw_word ::= ANY
+            raw_word ::= zero
+            raw_word ::= one
+            raw_word ::= two
+            raw_word ::= three
+            raw_word ::= four
+            raw_word ::= five
+            raw_word ::= six
+            raw_word ::= seven
+            raw_word ::= eight
+            raw_word ::= nine
+        '''
+        if(args[0].type == 'ANY'):
+            return args[0].extra
+        return args[0].type
 
 class SingleInputParser(CoreParser):
     def __init__(self):
