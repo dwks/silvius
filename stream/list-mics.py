eea71f514fba5629db2c15d842e0a9125c73c4ee
dwk <dwks42@gmail.com>
2018-10-10 17:57:27 -0400

    Converting list-mics.py to python 3.

--- stream/list-mics.py
+++ stream/list-mics.py
@@ -2,20 +2,20 @@ import pyaudio
 
 pa = pyaudio.PyAudio()  # prints a lot of junk
 
-print ""
-print "LISTING OF ALL INPUT DEVICES SUPPORTED BY PORTAUDIO"
-print "(any device numbers not shown are for output only)"
-print ""
+print("")
+print("LISTING OF ALL INPUT DEVICES SUPPORTED BY PORTAUDIO")
+print("(any device numbers not shown are for output only)")
+print("")
 
 for i in range(0, pa.get_device_count()):
     info = pa.get_device_info_by_index(i)
 
     if info['maxInputChannels'] > 0:  # microphone? or just speakers
-        print "DEVICE #%d" % info['index']
-        print "    %s" % info['name']
-        print "    input channels = %d, output channels = %d, defaultSampleRate = %d" \
-            % (info['maxInputChannels'], info['maxOutputChannels'], info['defaultSampleRate'])
-        print info
+        print("DEVICE #%d" % info['index'])
+        print("    %s" % info['name'])
+        print("    input channels = %d, output channels = %d, defaultSampleRate = %d" \
+            % (info['maxInputChannels'], info['maxOutputChannels'], info['defaultSampleRate']))
+        print(info)
 #       try:
 #           supports16k = pa.is_format_supported(16000,  # sample rate
 #               input_device = info['index'],
